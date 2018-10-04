31092fda610d24f061b71b05a024d746d0b6b57c
patelarchit82 <patelarchit82@gmail.com>
2018-10-04 14:43:06 -0600

    oauth

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -12,7 +12,6 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestConfig {
 
-    @Bean
     @LoadBalanced
     public RestOperations restOperations() {
         return new RestTemplate();
